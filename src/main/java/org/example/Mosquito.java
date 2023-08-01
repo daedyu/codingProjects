84a2e24eb0a1e15224be444508822cfdaf9758ac
daedyu <ceodaedyu@gmail.com>
2023-08-01 12:22:02 +0900

    third commit

--- src/main/java/org/example/Mosquito.java
+++ src/main/java/org/example/Mosquito.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Main {
+public class Mosquito {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
